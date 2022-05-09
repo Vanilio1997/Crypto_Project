16063547896982f16cd93d5017e168dacb31a5b0
Vanilio1997 <Vlad.rabota.96@yandex.ru>
2022-05-09 22:07:43 +0300

    Add my project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './components/Header'
+import './App.css' 
+import Main from './components/Main'
+import {Route ,Routes} from 'react-router-dom';
+import Cryptocurrencies from './components/Cryptocurrencies'
+import Exchanger from './components/Exchanger'
+import News from './components/News'
+import  CryptoInfo  from './components/CryptoInfo';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <>
+ <Header />
+ <Routes>
+       <Route path='/' element={<Main />} />
+       <Route path='/cryptocurrencies' element={<Cryptocurrencies />} />
+       <Route path='/exchanger' element={<Exchanger />} />
+       <Route path='/news' element={< News/>} />
+       <Route path='/crypto/:coinId' element={<CryptoInfo />} />
+   </Routes>
+</>
+  )
 }
 
-export default App;
+export default App
